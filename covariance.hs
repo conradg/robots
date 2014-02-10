ed4826e2938646ab9b0b51cf4a740dc6719adfb6
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2014-02-10 12:24:08 +0000

    cov

--- covariance.hs
+++ covariance.hs
@@ -1,15 +1,16 @@
 import System.IO
 import System.Environment
+
 type Matrix a = (
                  (a, a)  
                 ,(a, a)
                 ) 
 main = do 
         args <- getArgs
-        let handle = case args of
-                        [x] -> openFile x ReadMode
-                        _   -> return stdin
-        file <- handle >>= hGetContents
+        let contents = case args of
+                        [x] -> readFile x 
+                        _   -> hGetContents stdin 
+        file <- contents
         let contents = map (\x -> case words x of 
                                     a:b:xs -> p (read a) (read b)
                                     _     -> error "invalid input format") $ lines file
@@ -21,11 +22,10 @@ main = do
 matrix :: a -> a -> a -> a -> Matrix a
 matrix x1 x2 x3 x4 = ((x1,x2),(x3,x4))
 
-
 covarianceMatrix :: [(Double,Double)] -> Matrix Double
 covarianceMatrix coords = matrix tl tr bl br
     where
-    len = fromIntegral $length coords
+    len = fromIntegral $ length coords
     avg xory cs = (sum (map xory cs)) / len
     avgx = avg fst coords
     avgy = avg snd coords
