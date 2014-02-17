6955855ad26f9859c5cd0a4ff76b041c7eed3e17
wrd11 <will.derenzymartin@gmail.com>
2014-02-17 12:25:49 +0000

    added pointCloud method

--- week3.py
+++ week3.py
@@ -14,45 +14,46 @@ theta = 0
 def getRandomError():
     return random.gauss(mu, sigma)
 
-numberOfParticles = 100
+def pointCloud():
+    numberOfParticles = 100
 
-side1 = (40, 40, 440, 40) # (x0, y0, x1, y1)
-side2 = (440, 40, 440, 440)  # (x0, y0, x1, y1)
-side3 = (440, 440, 40, 440) # (x0, y0, x1, y1)
-side4 = (40, 440, 40, 40)  # (x0, y0, x1, y1)
+    side1 = (40, 40, 440, 40) # (x0, y0, x1, y1)
+    side2 = (440, 40, 440, 440)  # (x0, y0, x1, y1)
+    side3 = (440, 440, 40, 440) # (x0, y0, x1, y1)
+    side4 = (40, 440, 40, 40)  # (x0, y0, x1, y1)
 
-print "drawLine:" + str(side1)
-print "drawLine:" + str(side2)
-print "drawLine:" + str(side3)
-print "drawLine:" + str(side4)
+    print "drawLine:" + str(side1)
+    print "drawLine:" + str(side2)
+    print "drawLine:" + str(side3)
+    print "drawLine:" + str(side4)
 
-i = 0
+    i = 0
 
-while True:
-    turning = False
-    i += 1
-    if i == 100:
-        turning = True
-    if i == 200:
+    while True:
         turning = False
+        i += 1
+        if i == 100:
+            turning = True
+        if i == 200:
+            turning = False
 
-    if turning :
-        thetaChange = 90
-        x = x
-        y = y
-        theta = theta + thetaChange + getRandomError()
-    else :
-        xChange = 10
-        yChange = 10
-        x = x + (xChange + getRandomError()) * math.cos(theta)
-        y = y + (yChange + getRandomError()) * math.sin(theta)
-        theta = theta + getRandomError()
+        if turning :
+            thetaChange = 90
+            x = x
+            y = y
+            theta = theta + thetaChange + getRandomError()
+        else :
+            xChange = 10
+            yChange = 10
+            x = x + (xChange + getRandomError()) * math.cos(theta)
+            y = y + (yChange + getRandomError()) * math.sin(theta)
+            theta = theta + getRandomError()
 
-    # Create a list of particles to draw. This list should be filled by tuples (x, y, theta).
-    particles = [(x, y, theta) for i in range(numberOfParticles)]
-    print "drawParticles:" + particles
+        # Create a list of particles to draw. This list should be filled by tuples (x, y, theta).
+        particles = [(x, y, theta) for i in range(numberOfParticles)]
+        print "drawParticles:" + particles
 
-    time.sleep(0.01)
+        time.sleep(0.01)
 
 
 def goTo (xnew,ynew):
@@ -67,4 +68,6 @@ def goTo (xnew,ynew):
 	y = ynew
 	theta = angle
 
-goTo(.3,.3)
+pointCloud()
+#goTo(.3,.3)
+
