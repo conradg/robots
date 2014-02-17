14287c2caaf90995d8d6a5d22b601932db7b8ede
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2014-02-17 18:16:45 +0000

    need to finish pointcloud

--- week3.py
+++ week3.py
@@ -3,7 +3,10 @@ import time
 import random
 
 mu = 0 # mean (no error)
-sigma = 50 #standard dev (possible error)
+sigma = 0.5 #standard dev (possible error)
+
+DISPLAY_SQUARE_MARGIN = 50
+DISPLAY_SQUARE_SIDE = 500
 
 x = 0
 y = 0
@@ -12,46 +15,50 @@ theta = 0
 def getRandomError():
     return random.gauss(mu, sigma)
 
-def pointCloud():
-    x = 40
-    y = 440
-    theta = 90
+def recalculatePointCloud(particles, dx, dy, dtheta):
+    out = []
+    for particle in particles:
+        x, y , theta = particle
+        if dtheta :
+            theta = theta + dtheta + getRandomError()
+        else:
+            x = x + (dx + getRandomError()) * math.cos(theta)
+            y = y + (dy + getRandomError()) * math.sin(theta)
+            theta = theta + getRandomError()
+        x = int(x)
+        y = int(y)
+        out.append((x,y,theta))
+    return out
+
+
+def pointCloud(x,y,theta):
     numberOfParticles = 10
 
-    side1 = (40, 40, 440, 40)    # (x0, y0, x1, y1)
-    side2 = (440, 40, 440, 440)  # (x0, y0, x1, y1)
-    side3 = (440, 440, 40, 440)  # (x0, y0, x1, y1)
-    side4 = (40, 440, 40, 40)    # (x0, y0, x1, y1)
+    dsm = DISPLAY_SQUARE_MARGIN
+    dss = DISPLAY_SQUARE_SIDE
+    side1 = (dsm, dsm, dss+dsm, dsm)    # (x0, y0, x1, y1)
+    side2 = (dss+dsm, dsm, dss+dsm, dss+dsm)  # (x0, y0, x1, y1)
+    side3 = (dss+dsm, dss+dsm, dsm, dss+dsm)  # (x0, y0, x1, y1)
+    side4 = (dsm, dss+dsm, dsm, dsm)    # (x0, y0, x1, y1)
 
     print "drawLine:" + str(side1)
     print "drawLine:" + str(side2)
     print "drawLine:" + str(side3)
     print "drawLine:" + str(side4)
 
-    i = 0
-    particles = [(x,y,theta) for particle in range(numberOfParticles)]
-    for particle in range(numberOfParticles):
-        turning = False
-        i += 100
-        if i == 100:
-            turning = True
-        if i == 200:
-            turning = False
-
-        if turning:
-            thetaChange = 90
-            x = x
-            y = y
-            theta = theta + thetaChange + getRandomError()
-        else :
-            xChange = 10
-            yChange = 10
-            x = x + (xChange + getRandomError()) * math.cos(theta)
-            y = y + (yChange + getRandomError()) * math.sin(theta)
-            theta = theta + getRandomError()
-        # Create a list of particles to draw. This list should be filled by tuples (x, y, theta).
-    print "drawParticles:" , particles
+    pointcloud = [(dsm,dss+dsm,0) for j in range(numberOfParticles)]
+    print "drawParticles:" , pointcloud
+    for i in range(100):
+        pointcloud = recalculatePointCloud(pointcloud,5,0,0)
+        print "drawParticles:" , pointcloud
+        time.sleep(0.1)
+    pointcloud = recalculatePointCloud(pointcloud,0,0,math.pi/2)
+    print "drawParticles:" , pointcloud
     time.sleep(0.1)
+    for i in range(100):
+        pointcloud = recalculatePointCloud(pointcloud,0,-5,0)
+        print "drawParticles:" , pointcloud
+        time.sleep(0.1)
 
 
 def goTo (xnew,ynew):
@@ -70,7 +77,7 @@ def goTo (xnew,ynew):
     y = ynew
     theta = angle
 
-pointCloud()
+pointCloud(0,0,0)
 #goTo(.3,.3)
 #goTo(0,0)
 
